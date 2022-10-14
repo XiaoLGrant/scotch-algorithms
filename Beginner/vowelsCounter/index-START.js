227f31cd2d5b78a4e0bb437e2c3ed3e55ae50bd9
Xiao Grant <xiao.grant93@gmail.com>
2022-10-13 18:38:13 -0700

    completed exercise

--- Beginner/vowelsCounter/index-START.js
+++ Beginner/vowelsCounter/index-START.js
@@ -1,11 +1,34 @@
 /* CHALLENGE
-Given a string of text, return the number of vowels found within the text
+Given a string of text containing 0 or more vowels, return the number of vowels found within the text
 e.g vowelsCounter('anehizxcv') // will return 3
 */
 
 
 function vowelsCounter(text) {
-    // Code goes here
+    // loop
+    /*let count = 0
+    const vowels = 'aeiou'
+    //for (let i = 0; i < text.length; i++) { // 2.488s
+    for (char of text.toLowerCase()) { //2.231s
+        if (vowels.includes(char)) {
+       // if (vowels.includes(text[i])) {
+            count ++
+        }
+    }
+    return count*/
+
+    //filter-- 2.04s
+    //return [...text].filter(char => 'aeiou'.includes(char.toLowerCase())).length()
+
+    //regex-- 2.503s
+    // const matches = text.match(/[aeiou]/gi)
+    // if (matches) {
+    //     return matches.length
+    // } else {
+    //     return 0
+    // }
+
+
 }
 
 
